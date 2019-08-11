84770e313b144e14cb03317b36c8b1fbce969689
thinkingflower <flower.negitoro12@gmail.com>
2019-08-11 19:51:19 +0900

    debag of rotate NAME

--- rotation.js
+++ rotation.js
@@ -1,13 +1,13 @@
-var name = document.getElementById('name');
+var nameElement = document.getElementById('nameElement');
 var deg = 0;
 function rotateName() {
     deg = deg + 6;
     deg = deg % 360;
     if((0 <= deg && deg < 90) || (270 <= deg && deg < 360)) {
-    name.className = 'face';
+    nameElement.className = 'face';
     }else {
-    name.className = 'back';
+    nameElement.className = 'back';
     }
-    name.style = 'rotateX(' + deg +  'deg)';
+    nameElement.style = 'transform:rotateX(' + deg +  'deg)';
 }
 setInterval(rotateName, 20);
